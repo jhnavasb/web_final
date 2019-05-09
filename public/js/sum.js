f88ccc26c8e54bfa7c1fd71bc45bbb3cfa8cea8b
Jhonatan Navas <jhonatannavas@usantotomas.edu.co>
2019-05-09 17:02:00 -0500

    Testing

--- public/js/sum.js
+++ public/js/sum.js
@@ -1,23 +1,12 @@
 (function ($) {
     "use strict";    
     
-    $('.btn btn-primary mb-2').on('click', function () {
-        var a = parseInt($('#n1').val(), 10),
-            b = parseInt($('#n2').val(), 10);
-
-        $('#out').val(a + b);
-    });
-
     $(document).ready(function(){
-        alert("funciona");
         $("#submit").on("click", function(){
-            var a = parseInt($('#n1').val());
-            var b = parseInt($('#n2').val());
-               var sum = a + b;
-                alert(sum);
+            var a = parseInt($('#n1').val()),
+                b = parseInt($('#n2').val());
+            $('#out').val(a + b);
         })
-    })
-
-    
+    })    
 
 })(jQuery);
